e8accbb01b7df442fac77963986fa03832b1fc24
Steffen Zieger <github@saz.sh>
2020-05-06 16:01:04 +0200

    ignore events of newly connected devices

--- mic2ha.py
+++ mic2ha.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import re
+import time
 import pyinotify
 import requests
 import click
@@ -8,10 +9,11 @@ from subprocess import check_output, CalledProcessError, DEVNULL
 
 
 class EventHandler(pyinotify.ProcessEvent):
-    def __init__(self, webhook, ssid, entity_id, pevent=None, **kwargs):
+    def __init__(self, webhook, ssid, entity_id, seconds, pevent=None, **kwargs):
         self.webhook = webhook
         self.ssid = ssid
         self.entity_id = entity_id
+        self.seconds = seconds
         super().__init__(pevent, **kwargs)
 
     def _check_ssid(self):
@@ -32,11 +34,7 @@ class EventHandler(pyinotify.ProcessEvent):
     def get_entity_domain(self):
         return self.entity_id.split(".")[0]
 
-    def _get_payload(self, maskname):
-        if maskname == "IN_CLOSE_WRITE":
-            action = "turn_off"
-        elif maskname == "IN_OPEN":
-            action = "turn_on"
+    def _get_payload(self, action):
         return {
             "service": self.get_entity_domain(),
             "entity_id": self.entity_id,
@@ -49,13 +47,32 @@ class EventHandler(pyinotify.ProcessEvent):
             return "notified"
         return f"notify failed ({r.status_code}): {r.text}"
 
+    def _get_action(self, maskname):
+        if maskname == "IN_CLOSE_WRITE":
+            return "turn_off"
+        elif maskname == "IN_OPEN":
+            return "turn_on"
+        return False
+
     def process_default(self, event):
+        if event.maskname not in ("IN_OPEN", "IN_CLOSE_WRITE"):
+            return
+        # we are only interested in capture devices
+        m = re.search("^/dev/snd/pcm.*c$", event.pathname)
+        if not m:
+            return
         if not self._check_ssid():
             print("SSID {self.ssid} not connected, HomeAssistant notification skipped")
             return
-        result = self._notify_ha(self._get_payload(event.maskname))
+        # do not process events of files younger than <seconds> seconds
+        # avoids issues with hot-plugging a device
+        file_stat = os.stat(event.pathname)
+        if time.time() - file_stat.st_atime < self.seconds:
+            print(f"{event.pathname} younger than {self.seconds} seconds, skipping")
+            return
+        result = self._notify_ha(self._get_payload(self._get_action(event.maskname)))
         print(
-            f"Received {event.maskname} for path {event.path}, HomeAssistant {result}"
+            f"Received {event.maskname} for path {event.pathname}, HomeAssistant {result}"
         )
 
 
@@ -78,13 +95,23 @@ class EventHandler(pyinotify.ProcessEvent):
     default=False,
     envvar="SSID",
 )
-def main(webhook_url, entity_id, ssid):
-    path = "/dev/snd/pcm*c"
+@click.option(
+    "--seconds",
+    help="Ignore events of files younger than this number of seconds",
+    default=5,
+    envvar="SECONDS",
+)
+def main(webhook_url, entity_id, ssid, seconds):
+    path = "/dev/snd"
     wm = pyinotify.WatchManager()
     wm.add_watch(
-        path=path, mask=pyinotify.IN_OPEN | pyinotify.IN_CLOSE_WRITE, do_glob=True
+        path=path,
+        mask=pyinotify.IN_OPEN | pyinotify.IN_CLOSE_WRITE,
+        do_glob=True,
+        auto_add=True,
+        rec=True,
     )
-    eh = EventHandler(webhook=webhook_url, ssid=ssid, entity_id=entity_id)
+    eh = EventHandler(webhook=webhook_url, ssid=ssid, entity_id=entity_id, seconds=seconds)
     notifier = pyinotify.Notifier(wm, eh)
     print(f"Watching all items matching {path}")
     notifier.loop()
